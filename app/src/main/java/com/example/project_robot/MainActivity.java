c4df1242acad52228c02dc9a65a98bb71c087663
Leandra00000 <leandra.sequeira@tecnico.ulisboa.pt>
2024-03-10 00:00:16 +0000

    ,

--- app/src/main/java/com/example/project_robot/MainActivity.java
+++ app/src/main/java/com/example/project_robot/MainActivity.java
@@ -1,12 +1,24 @@
 package com.example.project_robot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity implements Joystick.JoystickListener{
 
-    Joystick joystick;
+    private Joystick joystick;
+    private DatabaseReference mDatabase;
+    private ImageView Image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +26,33 @@ public class MainActivity extends AppCompatActivity implements Joystick.Joystick
         setContentView(R.layout.activity_main);
 
         joystick = findViewById(R.id.Joystick);
+        Image= findViewById(R.id.imageView);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        readImageFromDatabase();
     }
 
     @Override
     public void onJoystickMoved() {
         float xPercent=joystick.getX();
         float yPercent=joystick.getY();
-        Log.d("Left Joystick", "X percent: " + xPercent + " Y percent: " + yPercent);
+        mDatabase.child("Joystick").child("Xpercentage").setValue(xPercent);
+        mDatabase.child("Joystick").child("Ypercentage").setValue(yPercent);
+    }
+
+    private void readImageFromDatabase() {
+        mDatabase.child("Camera").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String imageUrl = snapshot.getValue(String.class);
+                Image.setVisibility(View.VISIBLE);
+                Picasso.get().load(imageUrl).fit().centerCrop().into(Image);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
